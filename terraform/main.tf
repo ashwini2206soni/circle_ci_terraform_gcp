a6176668b41af29659d71f50e23be86e2780bed2
ashwinisoni2206 <ashwini19soni@gmail.com>
2021-09-29 12:34:35 -0400

    rm bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,4 @@
-resource "google_storage_bucket" "dummy_bucket" {
-  name          = var.bucket_name
-  location      = "US"
-  force_destroy = true
-}
+
 resource "google_storage_bucket" "halal_bucket" {
   name          = "halalkaro"
   location      = "US"
