8cd078536c0ae4d86ae5542946eb6a88f59f9d7f
ashwinisoni2206 <ashwini19soni@gmail.com>
2021-09-29 12:33:13 -0400

    testing state

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "google_storage_bucket" "dummy_bucket" {
   location      = "US"
   force_destroy = true
 }
-resource "google_storage_bucket" "dummy_bucket" {
+resource "google_storage_bucket" "halal_bucket" {
   name          = "halalkaro"
   location      = "US"
   force_destroy = true
